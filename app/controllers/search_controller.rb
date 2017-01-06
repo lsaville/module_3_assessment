0f0b2537c60f001bf0d2e98754c76343eea6ac76
Lee Saville <l.w.saville@gmail.com>
2017-01-06 12:03:17 -0700

    Make store objects to pass to view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,5 +4,8 @@ class SearchController < ApplicationController
 		key = ENV['key']
     api_response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=phone,storeType,distance,name,city&pageSize=100&apiKey=#{key}")
 		parsed_response = JSON.parse(api_response.body, symbolize_names: true)[:stores]
+    @stores = parsed_response.map do |raw_store|
+      BestBuyStore.new(raw_store)
+    end
   end
 end
