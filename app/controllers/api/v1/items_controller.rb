c316a60f733d7cf68db9d21f6b223fd8a479006d
Lee Saville <l.w.saville@gmail.com>
2017-01-06 10:45:06 -0700

    Add content of create action with item_params

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,6 +7,15 @@ class Api::V1::ItemsController < ActionController::Base
     render json: Item.find_by(params[:id])
   end
 
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item, status: 201
+    else
+      render status: 400
+    end
+  end
+
   def destroy
     item = Item.find_by(params[:id])
     if item.delete
@@ -15,4 +24,9 @@ class Api::V1::ItemsController < ActionController::Base
       render nothing: true, status: 400
     end
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :description, :image_url)
+    end
 end
