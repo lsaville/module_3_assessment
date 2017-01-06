6e7574ccb7c791c312baafec71f6e66749fa7a5e
Lee Saville <l.w.saville@gmail.com>
2017-01-06 11:43:40 -0700

    Add faraday request with url

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,8 @@
 class SearchController < ApplicationController
   def index
+		zip = params[:search]
+    api_response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=phone,storeType,distance,name,city&pageSize=100&#{ENV['key']}")
+		parsed_response = JSON.parse(api_response.body, symbolize_names: true)
+binding.pry
   end
 end
