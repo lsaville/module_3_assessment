94124003b548e1aa98973f7fe79b132c99bbde1d
Lee Saville <l.w.saville@gmail.com>
2017-01-09 12:54:08 -0700

    Add key to Faraday call in prep for moving to service model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,7 @@
 class SearchController < ApplicationController
   def index
 		zip = params[:search]
-		key = ENV['key']
-    api_response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=phone,storeType,distance,name,city&pageSize=100&apiKey=#{key}")
+    api_response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=phone,storeType,distance,name,city&pageSize=100&apiKey=#{ENV['key']}")
 		parsed_response = JSON.parse(api_response.body, symbolize_names: true)[:stores]
     @stores = parsed_response.map do |raw_store|
       BestBuyStore.new(raw_store)
