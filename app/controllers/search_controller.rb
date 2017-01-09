869bc18c9f986dfb8576a4359332cbce5eba5b84
Lee Saville <l.w.saville@gmail.com>
2017-01-09 13:01:07 -0700

    Add the getting of the stores from the BestBuyStore class method get_stores

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
-		zip = params[:search]
     api_response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=phone,storeType,distance,name,city&pageSize=100&apiKey=#{ENV['key']}")
 		parsed_response = JSON.parse(api_response.body, symbolize_names: true)[:stores]
+    @stores = BestBuyStore.get_stores(params[:search])
     @stores = parsed_response.map do |raw_store|
       BestBuyStore.new(raw_store)
     end
