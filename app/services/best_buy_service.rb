e3af0fe85f570dd4aa069212947022a475c6416e
Lee Saville <l.w.saville@gmail.com>
2017-01-09 15:07:57 -0700

    Fix spacing

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -5,7 +5,7 @@ class BestBuyService
 
   def store_finder
     api_response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=phone,storeType,distance,name,city&pageSize=100&apiKey=#{ENV['key']}")
-		JSON.parse(api_response.body, symbolize_names: true)[:stores]
+    JSON.parse(api_response.body, symbolize_names: true)[:stores]
   end
 
   private
