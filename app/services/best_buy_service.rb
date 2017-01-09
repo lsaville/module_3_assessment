fcf1e55d40bbaf9031d9c194d40a6d98431f7765
Lee Saville <l.w.saville@gmail.com>
2017-01-09 15:04:32 -0700

    Create app service directory

---
+++ app/services/best_buy_service.rb
@@ -0,0 +1,13 @@
+class BestBuyService
+  def initialize(zip)
+    @zip = zip
+  end
+
+  def store_finder
+    api_response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=phone,storeType,distance,name,city&pageSize=100&apiKey=#{ENV['key']}")
+		JSON.parse(api_response.body, symbolize_names: true)[:stores]
+  end
+
+  private
+    attr_reader :zip
+end
