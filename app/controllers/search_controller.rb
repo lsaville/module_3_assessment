442039e16d85b05c2ba538c6e892578f4f88a2d0
Lee Saville <l.w.saville@gmail.com>
2017-01-09 15:02:36 -0700

    Clean up the controller to use only the BestBuyStore model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,5 @@
 class SearchController < ApplicationController
   def index
-    api_response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=phone,storeType,distance,name,city&pageSize=100&apiKey=#{ENV['key']}")
-		parsed_response = JSON.parse(api_response.body, symbolize_names: true)[:stores]
     @stores = BestBuyStore.get_stores(params[:search])
-    @stores = parsed_response.map do |raw_store|
-      BestBuyStore.new(raw_store)
-    end
   end
 end
